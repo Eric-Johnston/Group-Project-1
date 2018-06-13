28a6b155a776a57d73b8472546b093636470f1c9
Jason Goeble <jason.goeble@gmail.com>
2018-06-13 13:19:06 -0400

    added firebase link to index.html, placed reset.css in the assets/css folder, made minor modification to the css links in index.html

---
+++ assets/javascript/gmaps.js
@@ -0,0 +1,11 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyByKZKEuCfRM-tdsjJiC_XIsiGdxOgGsgk",
+    authDomain: "concerts-events.firebaseapp.com",
+    databaseURL: "https://concerts-events.firebaseio.com",
+    projectId: "concerts-events",
+    storageBucket: "concerts-events.appspot.com",
+    messagingSenderId: "160710496348"
+  };
+  firebase.initializeApp(config);
+  
